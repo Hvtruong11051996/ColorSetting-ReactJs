1e08a24d99b8138dab480d46bd7b7649c52b3518
VINADES.,JSC <contact@vinades.vn>
2021-04-14 02:18:26 +0700

    Mini project ReactJs đầu tiên

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ColorPicker from './components/ColorPicker';
+import Reset from './components/Reset';
+import Result from './components/Result';
+import SizeSetting from './components/SizeSetting';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'red',
+            fontSize: 15
+        };
+    }
+    // Nhận dữ liệu từ con ->  cha thông qua function
+    onSetColor = (params) => {
+        this.setState({
+            color: params
+        });
+    }
+
+    onChangSize = (value) => {
+        if (this.state.fontSize + value >= 8 && this.state.fontSize + value <= 36) {
+            this.setState({
+                fontSize: this.state.fontSize + value
+            });
+        }
+    }
+
+    onSettingDefault = (value) => {
+        this.setState({
+            color: 'red',
+            fontSize: 15
+        });
+
+    }
+
+    render() {
+        return (
+            <div className='container mt-50'>
+                <div className='row'>
+                    <ColorPicker color={this.state.color} onReciveColor={this.onSetColor}></ColorPicker>
+                    <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <SizeSetting
+                            fontSize={this.state.fontSize}
+                            onChangSize={this.onChangSize}
+                        ></SizeSetting>
+                        <Reset onSettingDefault={this.onSettingDefault}></Reset>
+                    </div>
+                    <Result color={this.state.color} fontSize={this.state.fontSize}></Result>
+                </div>
+            </div>
+        );
+    };
 }
 
 export default App;
